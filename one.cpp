f8224bb4973886da89711d82f5f4bcacaf2e81d3
gopiembien <gopiembien@gmail.com>
2022-03-09 15:34:57 +0530

    m1

--- one.cpp
+++ one.cpp
@@ -2,8 +2,6 @@
 
 int main()
 {
-    std::cout<<"Good morning"<<std::endl;
-    std::cout<<"how are you"<<std::endl;
-    std::cout<<"This is test-02 branch"<<std::endl;
+    int m1;
     return 0;
 }
